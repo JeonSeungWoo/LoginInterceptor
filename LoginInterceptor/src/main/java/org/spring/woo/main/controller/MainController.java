7cd2937a22dd4c2462cc4fd8671090c5df998801
usdrd <usdrd@aqlsz>
2019-09-14 02:15:32 +0900

    a

--- LoginInterceptor/src/main/java/org/spring/woo/main/controller/MainController.java
+++ LoginInterceptor/src/main/java/org/spring/woo/main/controller/MainController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-
-	
-	
     @RequestMapping(value="/mainPage",method=RequestMethod.GET)
     public String loginForm(){
         return "/main/mainPage";
