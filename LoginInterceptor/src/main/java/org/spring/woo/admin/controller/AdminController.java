f133cbffde9e8f56460b32b2a166f3a7b7244089
usdrd <aqlsz@naver.com>
2019-09-14 02:10:11 +0900

    a

--- LoginInterceptor/src/main/java/org/spring/woo/admin/controller/AdminController.java
+++ LoginInterceptor/src/main/java/org/spring/woo/admin/controller/AdminController.java
@@ -1,5 +1,18 @@
 package org.spring.woo.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@RequestMapping("/admin/*")
+@Controller
 public class AdminController {
+	
+	 @RequestMapping(value="/adminMain",method=RequestMethod.GET)
+	    public String loginForm(){
+	        return "/admin/adminMain";
+	    }
+		
 
 }
